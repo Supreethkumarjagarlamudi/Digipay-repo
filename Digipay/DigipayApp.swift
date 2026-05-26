dda672ed25e9884caa675a9934eaf1cc26750333
Supreeth kumar <jsupreeth2005@gmail.com>
2026-05-26 08:55:35 +0530

    Initial Commit

---
+++ Digipay/DigipayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigipayApp.swift
+//  Digipay
+//
+//  Created by Supreeth kumar jagarlamudi on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DigipayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
